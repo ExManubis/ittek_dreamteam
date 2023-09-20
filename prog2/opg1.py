7d2101ff5c4787e067df32f08dcc9d59fb16f0db
Dukie666 <lucas1003lee@gmail.com>
2023-09-20 11:29:23 +0200

    Update opg1.py

--- prog2/opg1.py
+++ prog2/opg1.py
@@ -1,2 +1,11 @@
-# Test
-print('hello')
+import umqtt_robust2 as mqtt
+from machine import Pin
+
+led3 = Pin(13, Pin.OUT)
+
+while True:
+    try:
+        if mqtt.besked == "led3 on":
+            led3.on()
+        elif mqtt.besked == "led3 off":
+            led3.off()
