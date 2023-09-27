7d35b282f23bdbace508402649cf288ce8285c74
Mikkel Jørgensen <mikjorgensen@proton.me>
2023-09-27 13:38:47 +0200

    Create 1.py

---
+++ prog3/1.py
@@ -0,0 +1,9 @@
+from random import choice
+
+# 1.1 lav liste med 4 items
+StudieGruppe = ['Mikkel', 'Sofie', 'Alexis', 'Lucas']
+# 1.4 + 1.5 lav liste med 6 hvor 3 er ens.
+FavPokemon = ['Charizard', 'Mew', 'MewTwo', 'Pikachu', 'Pikachu', 'Pikachu']
+Dice = [1, 2, 3, 4, 5, 6] # terning
+
+print(choice(Dice), ',', choice(Dice))
